687029b84e8532adf8820ccd06c0ca12220dccd6
sriramandevakumar <sriramandevakumar@gmail.com>
2017-07-19 22:18:24 +0530

    Testcase
    Here  is a test case with hard coded value for the rate given in the rate file.

--- StockMaintance/src/Testcase/Jutest.java
+++ StockMaintance/src/Testcase/Jutest.java
@@ -2,7 +2,6 @@ package Testcase;
 
 import static org.junit.Assert.*;
 
-import java.io.IOException;
 import java.util.HashMap;
 
 import org.junit.Test;
@@ -10,14 +9,25 @@ import org.junit.Test;
 import StockMaintance.MainReadFile; 
   
 public class Jutest {  
-  HashMap<Double,String> hash11=new HashMap<Double,String>();
+  HashMap<Double,String> hash1=new HashMap<Double,String>();
+  HashMap<Double,String> hash2=new HashMap<Double,String>();
+
  
   
     @Test  
     public void testFindMax() throws Exception{  
-    	 hash11.put(12500.0,"GOOG - 100,AMZN - 90,MS - 80");
-    	 hash11.put(2500.0, "AMZN - 90,MS - 80");
-    	 hash11.put(900.0,"AMZN - 90" );
-        assertEquals(hash11,MainReadFile.ReadFile("test"));
+    	 hash1.put(12740.0,"GOOG - 100,AMZN - 90,MS - 80");
+    	 hash1.put(2740.0, "AMZN - 90,MS - 80");
+    	 hash1.put(900.0,"AMZN - 90" );
+    	 assertEquals(hash1,MainReadFile.ReadFile("test1.txt"));
+         hash2.put(1000000.0,"SGI-10000");
+         hash2.put(12740.0,"GOOG - 100,AMZN - 90,MS - 80");
+         hash2.put(5100.0,"GOOG - 20,SGI-20,GOOG -11");
+         assertEquals(hash2,MainReadFile.ReadFile("test.txt"));
+         assertEquals(null,MainReadFile.ReadFile("test"));//This will thrown as exception for extension
+         assertEquals(null,MainReadFile.ReadFile("test.txxxt"));//This will thrown as exception for correct extension
+         assertEquals(null,MainReadFile.ReadFile("txxxt.txt"));//This will thrown as exception for file not in location
+         
+         
     }  
 }  
